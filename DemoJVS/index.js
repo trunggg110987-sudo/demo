8bcd400d25df46df55b8bc191229949903555f97
trunggg110987-sudo <trunggg110987@gmail.com>
2025-10-14 16:25:23 +0700

    index.html

--- DemoJVS/index.js
+++ DemoJVS/index.js
@@ -1,3 +1,11 @@
-function showMessage() {
-    alert("Hello World!");
-}
+let i = 10
+
+let f = 20.5
+let b = true
+let s = 'Hà Nội'
+document.write('i = ' + i)
+document.write('<br/>')
+document.write('f = ' + f)
+document.write('<br/>')
+document.write('b = ' + b)
+document.write('<br/>')
