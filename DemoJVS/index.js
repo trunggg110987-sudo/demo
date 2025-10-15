c96105b2830a01bb3ede1816bbbe07b8e50dba42
trunggg110987-sudo <trunggg110987@gmail.com>
2025-10-15 20:38:05 +0700

    AppMayTinh.html

--- DemoJVS/index.js
+++ DemoJVS/index.js
@@ -1,10 +1,2 @@
-let vatly= prompt("nhập điểm vật lý");
-let hoahoc= prompt("nhập điểm hóa học");
-let sinhhoc= prompt("nập điểm sinh học");
-
-let tong = vatly + hoahoc + sinhhoc
-let diemTB = (vatly + hoahoc + sinhhoc)/3
-document.write(diemTB)
-document.write(tong)
-
-
+dvsv
+dsdvsd
